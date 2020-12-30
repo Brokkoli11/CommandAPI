fffab433df757053a2102b57b0e1130eb6e29475
Brokkoli11 <varszegi.mark111@gmail.com>
2020-12-30 13:33:41 +0100

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>>Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+
+    }
+}
